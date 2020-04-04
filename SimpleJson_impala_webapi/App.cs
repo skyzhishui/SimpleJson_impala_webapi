7c0d3fef5c590234df0d1b257a824ca2b4915596
skyzhishui <skyzhishui@qq.com>
2020-04-04 21:04:02 +0800

    Update App.cs

--- SimpleJson_impala_webapi/App.cs
+++ SimpleJson_impala_webapi/App.cs
@@ -90,23 +90,6 @@ namespace SimpleJson_impala_webapi
             }
         }
     }
-    public static class JsonUntity
-    {
-        public static string SerializeDictionaryToJsonString<TKey, TValue>(Dictionary<TKey, TValue> dict)
-        {
-            if (dict.Count == 0)
-                return "";
-            string jsonStr = JsonConvert.SerializeObject(dict);
-            return jsonStr;
-        }
-        public static Dictionary<TKey, TValue> DeserializeStringToDictionary<TKey, TValue>(string jsonStr)
-        {
-            if (string.IsNullOrEmpty(jsonStr))
-                return new Dictionary<TKey, TValue>();
-            Dictionary<TKey, TValue> jsonDict = JsonConvert.DeserializeObject<Dictionary<TKey, TValue>>(jsonStr);
-            return jsonDict;
-        }
-    }
     public class TimeserieResultModel
     {
         public string target { get; set; }
